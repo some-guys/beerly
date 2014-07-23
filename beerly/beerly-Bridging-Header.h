395a0e0aef15c8a397b0d5f83cab3bd1e15d2246
jruef <j.ruef@fadendaten.ch>
2014-07-23 12:02:40 +0200

    added bridging header for afnetworking

---
+++ beerly/beerly-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  beerly-Bridging-Header.h
+//  beerly
+//
+//  Created by Jonas Ruef on 23.07.14.
+//  Copyright (c) 2014 some-guys. All rights reserved.
+//
+#import <AFNetworking/AFNetworking.h>
