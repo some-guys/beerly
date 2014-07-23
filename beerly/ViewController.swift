aa4fc0323dceb69c3b1df5bf95cd7b738986d79b
Felix Langenegger <felix.langenegger@gmail.com>
2014-07-23 12:58:21 +0200

    Added first Map for the iphone App
    Tutorial:
    http://www.appshocker.com/swift-programming-ios-101-part-4-mapkit-app-displaying-locations/

--- beerly/ViewController.swift
+++ beerly/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate{
+    @IBOutlet var MapView : MKMapView
+    
+
                             
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    var latitude:CLLocationDegrees = 9.44543
+    var longitude:CLLocationDegrees  = 12.2345
+    
+    var latitudeDelta:CLLocationDegrees = 0.01
+    var longitudeDelta:CLLocationDegrees = 0.01
+    
+    var span:MKCoordinateSpan = MKCoordinateSpanMake(latitudeDelta, longitudeDelta)
+    var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+    var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+    
+    self.MapView.setRegion(region, animated: true)
+
+
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+    
 
 
 }
