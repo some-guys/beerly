78d8c945b7a8cfe5342abd51565489694a634b94
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2014-07-23 12:07:33 +0200

    Added location model

---
+++ beerly/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  beerly
+//
+//  Created by Rathesan Iyadurai on 23/07/14.
+//  Copyright (c) 2014 some-guys. All rights reserved.
+//
+
+import Cocoa
+
+class Location: NSObject {
+
+}
